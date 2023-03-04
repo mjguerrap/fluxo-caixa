ccff0d6043e31dfe0c33acc450b8e930a556a636
marcelopereira <mjguerrap@gmail.com>
2023-03-04 12:02:03 -0300

    removendo prefixo desnecessãrio

--- FluxoCaixa/Servicos/LancamentoService.cs
+++ FluxoCaixa/Servicos/LancamentoService.cs
@@ -7,34 +7,34 @@ namespace FluxoCaixa.Servicos;
 
 public class LancamentoService : ILancamentoService
 {
-    private readonly FluxoCaixaContext _fluxoCaixaDb;
+    private readonly FluxoCaixaContext fluxoCaixaDb;
 
-    public LancamentoService(FluxoCaixaContext fluxoCaixaDb) => _fluxoCaixaDb = fluxoCaixaDb;
+    public LancamentoService(FluxoCaixaContext fluxoCaixaDb) => this.fluxoCaixaDb = fluxoCaixaDb;
 
     public async Task<Lancamento?> Ajustar(int id, LancamentoPayload lancamento)
     {
-        var lancamentoToUpdate = await _fluxoCaixaDb.Lancamentos.FindAsync(id);
+        var lancamentoToUpdate = await fluxoCaixaDb.Lancamentos.FindAsync(id);
         if (lancamentoToUpdate is null) return null;
         lancamentoToUpdate.Atuazliza(lancamento.Descricao, lancamento.TipoLancamento, lancamento.Data, lancamento.Valor);
 
-        await _fluxoCaixaDb.SaveChangesAsync();
+        await fluxoCaixaDb.SaveChangesAsync();
         return lancamentoToUpdate;
     }
 
     public async Task<Lancamento> Novo(LancamentoPayload lancamento)
     {
         var lancamentoToAdd = new Lancamento(0, lancamento.Descricao, lancamento.TipoLancamento, lancamento.Data, lancamento.Valor);
-        await _fluxoCaixaDb.Lancamentos.AddAsync(lancamentoToAdd);
-        await _fluxoCaixaDb.SaveChangesAsync();
+        await fluxoCaixaDb.Lancamentos.AddAsync(lancamentoToAdd);
+        await fluxoCaixaDb.SaveChangesAsync();
         return lancamentoToAdd;
     }
     public async Task<Lancamento?> Remover(int id)
     {
-        var lancamentoToRemove = await _fluxoCaixaDb.Lancamentos.FindAsync(id);
+        var lancamentoToRemove = await fluxoCaixaDb.Lancamentos.FindAsync(id);
         if (lancamentoToRemove is null) return null;
 
-        _fluxoCaixaDb.Lancamentos.Remove(lancamentoToRemove);
-        await _fluxoCaixaDb.SaveChangesAsync();
+        fluxoCaixaDb.Lancamentos.Remove(lancamentoToRemove);
+        await fluxoCaixaDb.SaveChangesAsync();
         return lancamentoToRemove;
     }
 
@@ -42,11 +42,11 @@ public class LancamentoService : ILancamentoService
     {
         int tamanhoPagina = 1000;
         int pular = tamanhoPagina * (pagina == 0 ? 0 : pagina-1);
-        return await _fluxoCaixaDb.Lancamentos.Where(x => x.Data.Year == ano && x.Data.Month == mes).Skip(pular).Take(tamanhoPagina).ToListAsync();
+        return await fluxoCaixaDb.Lancamentos.Where(x => x.Data.Year == ano && x.Data.Month == mes).Skip(pular).Take(tamanhoPagina).ToListAsync();
     } 
     
       
     public async Task<Lancamento?> Buscar(int id) => 
-        await _fluxoCaixaDb.Lancamentos.FindAsync(id);
+        await fluxoCaixaDb.Lancamentos.FindAsync(id);
     
 }
